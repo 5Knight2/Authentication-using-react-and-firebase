7ff0967927cc7ae945ec17a9981b0b8f825e3343
5Knight2 <pratiktarale258@gmail.com>
2024-07-29 11:04:11 +0530

    Password auto reset done

---
+++ index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+import AuthProvider from './store/auth-provider';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AuthProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>   </AuthProvider>
+);
--- src/store/auth-provider.js
+++ src/store/auth-provider.js
@@ -1,17 +1,20 @@
 import AuthContext from "./auth-context";
 
 import React,{useState} from 'react'
+import { useHistory } from 'react-router-dom';
 
 const AuthProvider=(props)=>{
     const initialToken=localStorage.getItem('token')
 const [token,setToken]=useState(initialToken);
 const [loggedIn,setLoggedIn]=useState(false);
+const history=useHistory();
 
 const setTokenHandler=(data)=>{
     setToken(data)
     setLoggedIn(true)
     console.log('Setting Token')
     localStorage.setItem('token',token)
+    setTimeout(()=>{logoutHandler(); },60000*5)
 }
 
 
@@ -19,7 +22,7 @@ const logoutHandler=()=>{
      setToken('');
      setLoggedIn(false)
      localStorage.removeItem('token')
-
+     history.replace('/auth')
 }
 
 const auth={
